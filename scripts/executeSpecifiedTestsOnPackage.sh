f30a2928fbfe17561e9c797bedd82ee46a1c7b97
Manu V <106539515+manvil95@users.noreply.github.com>
2024-05-20 08:21:30 +0200

    Add script executeSpecifiedTests

---
+++ scripts/executeSpecifiedTestsOnPackage.sh
@@ -0,0 +1,30 @@
+# ------------------- Variable para especificar el nombre de la org según VS Code
+DESTINATION="<nameORG>";
+
+# ------------------- Especificar Package.xml, solo debe contener clases APEX y nomenclatura para Test.
+# ------------------- Ej.: <nombreClase>_Test, <nombreClase>Test
+FILE_PACKAGE="manifest/Package.xml";
+
+#------------ Variable para montar el comando de ejecucion de clases especificas del Test
+APEXTEST_LIST="";
+
+while read line;
+	do
+		if [[ $line == *'Test'* ]];
+		then
+			line=${line/"<members>"/""};
+			line=${line/"</members>"/","};
+			if [[ $line != *'DataTestFactory'* ]];
+			then
+				APEXTEST_LIST="$APEXTEST_LIST$line";
+			fi
+		fi
+	done < $FILE_PACKAGE
+	
+APEXTEST_LIST=${APEXTEST_LIST%,};
+
+# ------------------- Imprimimos el comando lanzado en la terminal
+echo "sfdx force:source:deploy -x "${FILE_PACKAGE}" -l RunSpecifiedTests -r "${APESXTEST_LIST}" -c -u "${DESTINATION}" --verbose";
+
+# ------------------- Comando sfdx para validar clases Apex ejecutando Test Especificos
+sfdx force:source:deploy -x ${FILE_PACKAGE} -l RunSpecifiedTests -r ${APESXTEST_LIST} -c -u ${DESTINATION} --verbose;
