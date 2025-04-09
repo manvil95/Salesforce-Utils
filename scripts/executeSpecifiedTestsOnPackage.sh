b553b26944dd1bfbd3f365d41a1cfa82a40b4354
Manuel Villen <manuvillen@gmail.com>
2025-04-09 20:04:53 +0200

    Update executeSpecifiedTestsOnPackage v2

--- scripts/executeSpecifiedTestsOnPackage.sh
+++ scripts/executeSpecifiedTestsOnPackage.sh
@@ -6,26 +6,47 @@ DESTINATION="<nameORG>";
 # ------------------- Ej.: <nombreClase>_Test, <nombreClase>Test
 FILE_PACKAGE="manifest/Package.xml";
 
+# ------------------- Configuración adicional (opcional)
+WAIT_TIME=33 # minutos para esperar la ejecución
+COVERAGE_FORMATTERS="json-summary" # formato para reportes de cobertura
+RESULTS_DIR="test-results" # directorio para resultados
+
 #------------ Variable para montar el comando de ejecucion de clases especificas del Test
 APEXTEST_LIST="";
 
-while read line;
-	do
-		if [[ $line == *'Test'* ]];
-		then
-			line=${line/"<members>"/""};
-			line=${line/"</members>"/","};
-			if [[ $line != *'DataTestFactory'* ]];
-			then
-				APEXTEST_LIST="$APEXTEST_LIST$line";
-			fi
-		fi
-	done < $FILE_PACKAGE
-	
-APEXTEST_LIST=${APEXTEST_LIST%,};
+# Procesar Package.xml para obtener tests
+while read line; do
+    if [[ $line == *'Test'* ]]; then
+        line=${line/"<members>"/""};
+        line=${line/"</members>"/""};
+        if [[ $line != *'DataTestFactory'* ]]; then
+            # Formatear para el comando sf (sin comas, múltiples --tests)
+            APEXTEST_LIST="$APEXTEST_LIST --tests $line";
+        fi
+    fi
+done < $FILE_PACKAGE
 
-# ------------------- Imprimimos el comando lanzado en la terminal
-echo "sfdx force:source:deploy -x "${FILE_PACKAGE}" -l RunSpecifiedTests -r "${APEXTEST_LIST}" -c -u "${DESTINATION}" --verbose";
+# ------------------- Construir y mostrar el comando
+COMMAND="sf project deploy start \
+    --manifest ${FILE_PACKAGE} \
+    --test-level RunSpecifiedTests \
+    ${APEXTEST_LIST} \
+    --target-org ${DESTINATION} \
+    --dry-run \
+    --verbose \
+    --wait ${WAIT_TIME} \
+    --coverage-formatters ${COVERAGE_FORMATTERS} \
+    --results-dir ${RESULTS_DIR} \
+    --junit";
 
-# ------------------- Comando sfdx para validar clases Apex ejecutando Test Especificos
-sfdx force:source:deploy -x ${FILE_PACKAGE} -l RunSpecifiedTests -r ${APEXTEST_LIST} -c -u ${DESTINATION} --verbose;
+echo "Comando a ejecutar:";
+echo $COMMAND;
+
+# ------------------- Ejecutar el comando
+eval $COMMAND;
+
+# ------------------- Opcional: Mostrar resumen si hay resultados JUnit
+if [ -f "${RESULTS_DIR}/junit/junit.xml" ]; then
+    echo "=== Resumen de Tests JUnit ===";
+    grep -E "(testsuite|testcase|failure)" "${RESULTS_DIR}/junit/junit.xml";
+fi
